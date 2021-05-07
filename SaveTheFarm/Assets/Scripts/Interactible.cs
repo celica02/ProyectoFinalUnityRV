2435b0bfefaab1530d8156d4a5ee45b0367e606f
Celia <celica02@ucm.es>
2021-05-07 22:34:01 +0200

    Añadidas herramientas y zonas de cultivo con sus estados (Temporal)

--- SaveTheFarm/Assets/Scripts/Interactible.cs
+++ SaveTheFarm/Assets/Scripts/Interactible.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Interactible : MonoBehaviour
 {
-    private GameObject[] states = new GameObject[3];
-    private int[] tools = new int[2];
+    private GameObject[] states = new GameObject[4];
+    private GameObject wateredSoil;
+    private int[] tools = new int[3];
+    private int idWater = 3;
     GameObject obj;
     int currentState = 0;
 
@@ -13,36 +15,49 @@ public class Interactible : MonoBehaviour
     void Start()
     {
         //Carga de Assets
-        states[1] = Resources.Load("Prefabs/Hole") as GameObject;
-        states[2] = Resources.Load("Prefabs/Dirt") as GameObject;
+        states[0] = Resources.Load("Prefabs/weeds") as GameObject;
+        states[1] = Resources.Load("Prefabs/weeds") as GameObject;
+        states[2] = Resources.Load("Prefabs/Hole") as GameObject;
+        states[3] = Resources.Load("Prefabs/Dirt") as GameObject;
 
-        tools[0] = tools[1] = 1;
+        wateredSoil = Resources.Load("Prefabs/WateredSoil") as GameObject;
+
+        tools[0] = tools[1] = tools[2] = 1;
         //Inicialización
         currentState = 0;
         if (states[currentState] != null)
+        {
             obj = Instantiate(states[currentState]);
+            obj.transform.position = new Vector3(gameObject.transform.position.x,
+                                                     obj.transform.position.y,
+                                                     gameObject.transform.position.z);
+        }
     }
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "Tool" &&
-            currentState < tools.Length && 
-            col.gameObject.GetComponent<Tool>().id == tools[currentState]
-            )
+        if (col.gameObject.tag == "Tool")
         {
-            if (obj != null)
-                Destroy(obj);
-            currentState++;
-            if (states[currentState] != null)
+            if (currentState < tools.Length &&
+               col.gameObject.GetComponent<Tool>().id == tools[currentState])
             {
-                obj = Instantiate(states[currentState]);
-                obj.transform.position = new Vector3(gameObject.transform.position.x, 
-                                                     obj.transform.position.y, 
-                                                     gameObject.transform.position.z);
-            }
+                if (obj != null)
+                    Destroy(obj);
+                currentState++;
+                if (states[currentState] != null)
+                {
+                    obj = Instantiate(states[currentState]);
+                    obj.transform.position = new Vector3(gameObject.transform.position.x,
+                                                         obj.transform.position.y,
+                                                         gameObject.transform.position.z);
+                }
 
+            }
+            else if(col.gameObject.GetComponent<Tool>().id == idWater)
+            {
+                wateredSoil = Instantiate(wateredSoil); //Se queda para siempre así que hay ver cuándo queremos que vuelva a estar seco.
+            }
         }
-        //gameObject.GetComponent<Renderer>().material = b;
     }
 }
 
---
+++ SaveTheFarm/Assets/Scripts/ObstructionObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstructionObject : MonoBehaviour
+{
+    public int toolID;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Tool" &&
+            col.gameObject.GetComponent<Tool>().id == toolID
+            )
+        {
+            Destroy(gameObject);
+        }
+    }
+}
