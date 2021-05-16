d16e0794028e8e4591591707be29d44b1ecfda80
Celia <celica02@ucm.es>
2021-05-16 02:20:10 +0200

    Cultivos funcionales

---
+++ SaveTheFarm/Assets/Scripts/Crops.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crops : MonoBehaviour
+{
+    private Interactible i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = gameObject.GetComponent<Interactible>();
+        i.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length >
+            gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime)
+        {
+            gameObject.GetComponent<Interactible>().enabled = true;
+        }
+    }
+}
--- SaveTheFarm/Assets/Scripts/Interactible.cs
+++ SaveTheFarm/Assets/Scripts/Interactible.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Interactible : MonoBehaviour
 {
-    private GameObject[] states = new GameObject[4];
+    private GameObject[] states = new GameObject[5];
     private GameObject wateredSoil;
-    private int[] tools = new int[3];
+    private int[] tools = new int[4];
     private int idWater = 3;
     GameObject obj;
     int currentState = 0;
+    bool watered = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,13 @@ public class Interactible : MonoBehaviour
         //states[1] = Resources.Load("Prefabs/weeds") as GameObject;
         states[2] = Resources.Load("Prefabs/Hole") as GameObject;
         states[3] = Resources.Load("Prefabs/Dirt") as GameObject;
-        //states[4] = Resources.Load("Prefabs/Pumpkins_Growable") as GameObject;
+        states[4] = Resources.Load("Prefabs/Pumpkins_Growable") as GameObject;
 
         wateredSoil = Resources.Load("Prefabs/WateredSoil") as GameObject;
 
-        tools[0] = tools[1] = tools[2] = 1;
+        tools[0] = tools[1] =  1;
+        tools[2] = 2;
+        tools[3] = 3;
         //Inicialización
         currentState = 0;
         if (states[currentState] != null)
@@ -47,17 +50,31 @@ public class Interactible : MonoBehaviour
                 currentState++;
                 if (states[currentState] != null)
                 {
+                    if (currentState == 3 && watered)
+                        currentState++;
+
                     obj = Instantiate(states[currentState]);
                     obj.transform.position = new Vector3(gameObject.transform.position.x,
                                                          obj.transform.position.y,
                                                          gameObject.transform.position.z);
+
+                    if (currentState == 4 && watered)
+                        obj.GetComponent<Animator>().Play("Grow");
                 }
 
             }
-            else if(col.gameObject.GetComponent<Tool>().id == idWater)
+            else if (!watered && col.gameObject.GetComponent<Tool>().id == idWater)
             {
-                wateredSoil = Instantiate(wateredSoil); //Se queda para siempre así que hay ver cuándo queremos que vuelva a estar seco.
+                wateredSoil = Instantiate(wateredSoil); //Se queda para siempre así, hay ver cuándo queremos que vuelva a estar seco.
+                wateredSoil.transform.position = new Vector3(gameObject.transform.position.x,
+                                                             wateredSoil.transform.position.y,
+                                                             gameObject.transform.position.z);
+                watered = true;
+                if (currentState == 4)
+                    obj.GetComponent<Animator>().Play("Grow");
             }
+
+
         }
     }
 }
